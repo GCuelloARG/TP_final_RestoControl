36b9710cbe3df883a743b5392429df3a6cfc5ea4
FlorVarangot <majoreyes87@gmail.com>
2023-10-23 17:14:45 -0300

    Flor

--- detalle.h
+++ detalle.h
@@ -1,6 +1,22 @@
 #ifndef DETALLE_H_INCLUDED
 #define DETALLE_H_INCLUDED
 
+/*
+DetalleVenta
+	priv:	Producto prod
+		int cantidad
+		float subtotal
+	pub: 	cargar()
+		mostrar()
 
+ArchivoDetalles
+	priv:	char nombre[30]
+	pub:	constructor
+	leerRegistro()
+agregarRegistro()
+eliminarRegistro()
+modificarRegistro()
+buscarRegistro()
+*/
 
 #endif // DETALLE_H_INCLUDED
