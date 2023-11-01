bca9f216a124a6f3d0c525b6e15b65dab50ce8cd
FlorVarangot <majoreyes87@gmail.com>
2023-11-01 19:04:29 -0300

    Update rep.h
    Detalles pantalla ventas x mes

--- rep.h
+++ rep.h
@@ -12,25 +12,26 @@ void ventasPorMes(){
     ArchivoVenta arcVen("ventas.dat");
     int mm, aux,  i, cantReg, cont=0;
     float fact;
-
     cantReg=arcVen.contarRegistros();
-
-    cout<<"Ingrese el MES (mm) que desea reportar: "<<endl;
+    cout<<endl<<"Ingrese el MES (mm) que desea reportar: ";
     cin>>mm;
+    system("cls");
+    cout<<"\t   -  VENTAS MES "<<mm<<" 2023  -"<<endl;
     if(cantReg>0){
         for(i=0; i<cantReg; i++){
             ven=arcVen.leerRegistro(i);
             fec=ven.getFechaVenta();
             aux=fec.getMes();
             if(mm==aux){
+
                 ven.Mostrar();
                 cont++;
                 fact+=ven.getPrecioTotal();
             }
        }
     }
-       cout<<endl<<"Cantidad de ventas del mes de "<<mm<<" 2023: "<<cont<<endl;
-       cout<<"Total facturado: "<<fact<<endl;
+       cout<<endl<<"Cantidad de ventas del mes "<<mm<<" de 2023: "<<cont<<endl;
+       cout<<"Total facturado: $"<<fact<<endl<<endl;
 
        system("pause");
        system("cls");
